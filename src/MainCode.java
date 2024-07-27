d1c3ec4b2627a75b9b6c0aef279fc65225ca5ea0
baileykiehl <bdk45@txstate.edu>
2024-07-27 11:58:00 -0500

    pushing my version

--- src/MainCode.java
+++ src/MainCode.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class MainCode {
     public static void main(String[] args) {
         // Initialize library and items
         Library library = new Library();
