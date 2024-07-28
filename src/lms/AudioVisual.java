923fd6f45c9e5181de7316a1bf2bcc8f4e013d82
sameeraraza <raza.sugar.land@gmail.com>
2024-07-28 13:57:33 -0500

    Update AudioVisual.java

--- src/lms/AudioVisual.java
+++ src/lms/AudioVisual.java
@@ -1,9 +1,8 @@
 package lms;
 
 public class AudioVisual extends Item {
-    private String format;
-
-    // Getters and Setters
-    public String getFormat() { return format; }
-    public void setFormat(String format) { this.format = format; }
+	public AudioVisual(String title, int checkOutLength, double bookPrice, boolean canBeCheckedOut, int copyNumber) {
+        super(title, checkOutLength, bookPrice, canBeCheckedOut, copyNumber);
+    }
 }
+
