3ca27bbed5be244a94ab1a7d059aee1947c2b7e4
sameeraraza <raza.sugar.land@gmail.com>
2024-07-28 14:01:40 -0500

    Update User.java

--- src/lms/User.java
+++ src/lms/User.java
@@ -9,32 +9,101 @@ public abstract class User {
     private String phoneNumber;
     private String userID;
     private String libraryCardNumber;
+    private int maxItem;
     private int age;
-    private List<Loans> loans = new ArrayList<>();
-    private List<Fines> fines = new ArrayList<>();
-
-    // Getters and Setters
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    public String getUserID() { return userID; }
-    public void setUserID(String userID) { this.userID = userID; }
-
-    public String getLibraryCardNumber() { return libraryCardNumber; }
-    public void setLibraryCardNumber(String libraryCardNumber) { this.libraryCardNumber = libraryCardNumber; }
-
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
-
-    public List<Loans> getLoans() { return loans; }
-    public void setLoans(List<Loans> loans) { this.loans = loans; }
-
-    public List<Fines> getFines() { return fines; }
-    public void setFines(List<Fines> fines) { this.fines = fines; }
+    private ArrayList <String[]> numOfBooksCheckedOut;
+    private ArrayList <String[]> numOfAVCheckedOut;
+    public User(String libraryCardNumber, String name, int age, String address, 
+            String phoneNumber, int maxItems, ArrayList<String[]> numOfBooksCheckedOut, ArrayList<String[]> numOfAVCheckedOut) {
+    this.name = name;
+    this.age = age; // (?)
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.libraryCardNumber = libraryCardNumber;
+    this.maxItem = maxItem
+    this.numOfBooksCheckedOut = numOfBooksCheckedOut;
+    this.numOfAVCheckedOut = numOfAVCheckedOut;
 }
+
+public int getNumberOfItems() {
+    int numItems = numOfBooksCheckedOut.size() + numOfAVCheckedOut.size();
+    return numItems;
+}
+
+public ArrayList<String[]> getCheckedOutBooks() {
+    return numOfBooksCheckedOut;
+}
+public ArrayList<String[]> getAVCheckedOut() {
+    return numOfAVCheckedOut;
+}
+public void returnBook(String[] item) {
+	numOfBooksCheckedOut.remove(item);
+}
+
+public void returnAV(String[] item) {
+	numOfAVCheckedOut.remove(item);
+}
+
+public ArrayList<String[]> getCheckedOutAV() {
+    return numOfAVCheckedOut;
+}
+
+public String getName() {
+    return name;
+}
+
+public int getAge() { // (?)
+    return age;
+}
+
+public String getAddress() {
+    return address;
+}
+
+public String getPhoneNumber() {
+    return phoneNumber;
+}
+
+public String getLibraryCardNumber() {
+    return libraryCardNumber;
+}
+
+public int getmaxItem() {
+    return maxItem;
+}
+
+public void addBook(String title, String renewed, String dueDate) {
+    String[] book = {title, renewed, dueDate};
+    numOfBooksCheckedOut.add(book);
+}
+
+
+public void addAVItem(String title, String renewed, String dueDate) {
+    String[] audioVisualItem = {title, renewed, dueDate};
+    numOfAVCheckedOut.add(audioVisualItem);
+}
+
+public String toString() {
+    String books = "";
+    String av = "";
+    for (String[] item : numOfBooksCheckedOut) {
+        books += "Title: " + item[0] + " Checkout Date: " + item[1] + " Due Date: " + item[2] + "\n";
+    } 
+    for (String[] item : numOfAVCheckedOut) {
+        av += "Title: " + item[0] + " Checkout Date: " + item[1] + " Due Date: " + item[2] + "\n";
+    }
+
+    return ("Name: " + name + "\n" +
+            "Library Card Number: " + libraryCardNumber + "\n" +
+            "Age: " + age + "\n" +
+            "Address: " + address + "\n" +
+            "Phone number: " + phoneNumber + "\n" +
+            "Max Items: " + maxItem + "\n" +
+            books + av);
+
+}
+    
+
+   
+}
+
