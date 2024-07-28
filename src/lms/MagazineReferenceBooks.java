7cd509c0a7fa403429a4816af71370870f1ce6d7
sameeraraza <raza.sugar.land@gmail.com>
2024-07-28 15:56:56 -0500

    Update MagazineReferenceBooks.java

--- src/lms/MagazineReferenceBooks.java
+++ src/lms/MagazineReferenceBooks.java
@@ -1,7 +1,7 @@
 package lms;
 
 public class MagazineReferenceBooks extends Item {
-	 public MagazineReferenceBooks(String title, int checkOutLength, double bookPrice, boolean canBeCheckedOut, int copyNumber) {
-	        super(title, checkOutLength, bookPrice, canBeCheckedOut, copyNumber);
+	 public MagazineReferenceBooks(String title, int checkOutLength, double magazineRefPrice, boolean canBeCheckedOut, int copyNumber) {
+	        super(title, checkOutLength, magazineRefPrice, canBeCheckedOut, copyNumber);
 	    }
 }
