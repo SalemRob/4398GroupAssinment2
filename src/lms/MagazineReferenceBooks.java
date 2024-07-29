74219417b217075c8b22aa6bde28d164974449ea
sameeraraza <raza.sugar.land@gmail.com>
2024-07-29 13:36:08 -0500

    Update MagazineReferenceBooks.java

--- src/lms/MagazineReferenceBooks.java
+++ src/lms/MagazineReferenceBooks.java
@@ -1,7 +1,9 @@
 package lms;
 
 public class MagazineReferenceBooks extends Item {
-	 public MagazineReferenceBooks(String title, int checkOutLength, double magazineRefPrice, boolean canBeCheckedOut, int copyNumber) {
-	        super(title, checkOutLength, magazineRefPrice, canBeCheckedOut, copyNumber);
-	    }
+    private String format;
+
+    // Getters and Setters
+    public String getFormat() { return format; }
+    public void setFormat(String format) { this.format = format; }
 }
